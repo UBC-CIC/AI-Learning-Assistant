f79c14e85a2b841591aee728e6fe519485b25d3f
Aman <apraka01@student.ubc.ca>
2024-08-26 11:15:24 -0700

    fix: considering module_id instead of concept_name in S3 Bucket paths.

--- cdk/lambda/getFilesFunction.py
+++ cdk/lambda/getFilesFunction.py
@@ -21,23 +21,23 @@ def lambda_handler(event, context):
     query_params = event.get("queryStringParameters", {})
 
     course_name = query_params.get("course_name", "")
-    concept_name = query_params.get("concept_name", "")
+    module_id = query_params.get("module_id", "")
     module_name = query_params.get("module_name", "")
 
-    if not course_name or not concept_name or not module_name:
+    if not course_name or not module_id or not module_name:
         logger.error("Missing required parameters", extra={
             "course_name": course_name,
-            "concept_name": concept_name,
+            "module_id": module_id,
             "module_name": module_name
         })
         return {
             'statusCode': 400,
-            'body': json.dumps('Missing required parameters: course_name, concept_name, or module_name')
+            'body': json.dumps('Missing required parameters: course_name, module_id, or module_name')
         }
 
     try:
-        document_prefix = f"{course_name}/{concept_name}/{module_name}/documents/"
-        image_prefix = f"{course_name}/{concept_name}/{module_name}/images/"
+        document_prefix = f"{course_name}/{module_name}_{module_id}/documents/"
+        image_prefix = f"{course_name}/{module_name}_{module_id}/images/"
 
         document_files = list_files_in_s3_prefix(BUCKET, document_prefix)
         image_files = list_files_in_s3_prefix(BUCKET, image_prefix)

d06d08eba5f3fd57da6511a888efd6cb2fd1c726
Aman <apraka01@student.ubc.ca>
2024-10-23 13:12:36 -0700

    fix: fixed JSON loading error when secret value is plain text.

--- cdk/text_generation/src/main.py
+++ cdk/text_generation/src/main.py
@@ -17,17 +17,29 @@ logger = logging.getLogger()
 DB_SECRET_NAME = os.environ["SM_DB_CREDENTIALS"]
 REGION = os.environ["REGION"]
 
-def get_secret(secret_name):
-    # secretsmanager client to get db credentials
-    sm_client = boto3.client("secretsmanager")
-    response = sm_client.get_secret_value(SecretId=secret_name)["SecretString"]
-    secret = json.loads(response)
-    return secret
+def get_secret(secret_name, expect_json=True):
+    try:
+        # secretsmanager client to get db credentials
+        sm_client = boto3.client("secretsmanager")
+        response = sm_client.get_secret_value(SecretId=secret_name)["SecretString"]
+        
+        if expect_json:
+            return json.loads(response)
+        else:
+            print(response)
+            return response
+
+    except json.JSONDecodeError as e:
+        logger.error(f"Failed to decode JSON for secret {secret_name}: {e}")
+        raise ValueError(f"Secret {secret_name} is not properly formatted as JSON.")
+    except Exception as e:
+        logger.error(f"Error fetching secret {secret_name}: {e}")
+        raise
 
 ## GET SECRET VALUES FOR CONSTANTS
-BEDROCK_LLM_ID = get_secret(os.environ["BEDROCK_LLM_SECRET"])
-EMBEDDING_MODEL_ID = get_secret(os.environ["EMBEDDING_MODEL_SECRET"])
-TABLE_NAME = get_secret(os.environ["TABLE_NAME_SECRET"])
+BEDROCK_LLM_ID = get_secret(os.environ["BEDROCK_LLM_SECRET"], expect_json=False)
+EMBEDDING_MODEL_ID = get_secret(os.environ["EMBEDDING_MODEL_SECRET"], expect_json=False)
+TABLE_NAME = get_secret(os.environ["TABLE_NAME_SECRET"], expect_json=False)
 
 ## GETTING AMAZON TITAN EMBEDDINGS MODEL
 bedrock_runtime = boto3.client(

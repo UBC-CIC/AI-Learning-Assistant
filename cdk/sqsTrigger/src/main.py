6c9ec1302016989366ab5c37526e65577cb5ecff
Aman <apraka01@student.ubc.ca>
2024-12-19 14:11:35 -0800

    fix: updated Dockerfile to point to handler.

--- cdk/sqsTrigger/src/main.py
+++ cdk/sqsTrigger/src/main.py
@@ -1,6 +1,6 @@
 import json
 
-def lambda_handler(event, context):
+def handler(event, context):
     """
     Lambda function to process messages from SQS.
     Prints the received parameters for testing purposes.
